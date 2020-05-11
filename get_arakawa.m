7b7bb3af4570395f52513329464df2c37f80c01a
Casey Densmore <cdensmore101@gmail.com>
2020-05-11 12:20:03 -0400

    Arakawa Jacobian update

--- get_arakawa.m
+++ get_arakawa.m
@@ -3,21 +3,6 @@ function Jpz = get_arakawa(psi,zeta,nx,ny,dx,dy)
 Jpz = zeros(nx,ny);
 coeff = 1/(3*dx*dy);
 
-% for i = 1:nx
-%     ic = i + 1;
-%     for j = 1:ny
-%         jc = j + 1;
-     
-%         Jpz(i,j) = + zeta(ic-1,jc+1)*(2*psi(ic,jc+1) - psi(ic-1,jc) - psi(ic,jc-1)) + ... %z0
-%                    + zeta(ic-1,jc)*(psi(ic-1,jc+1) - psi(ic-1,jc-1)) + ... %z1
-%                    + zeta(ic-1,jc-1)*(psi(ic-1,jc) - psi(ic,jc-1)) + ... %z2
-%                    + zeta(ic,jc-1)*(psi(ic-1,jc-1) - psi(ic+1,jc-1)) + ... %z3
-%                    + zeta(ic+1,jc-1)*(psi(ic,jc-1) + psi(ic-1,jc) - 2*psi(ic+1,jc)) + ... %z4
-%                    + zeta(ic,jc+1)*(psi(ic+1,jc+1) - psi(ic-1,jc+1)) + ... %z6
-%                    + zeta(ic+1,jc+1)*(2*psi(ic+1,j) - 2*psi(ic,jc+1) - psi(ic-1,jc)) + ... %z7
-%                    + zeta(ic+1,jc)*(psi(ic+1,jc-1) - psi(ic+1,jc+1)); %z8
-
-
 for ic = 1:nx
     i = ic + 1;
     for jc = 1:ny
